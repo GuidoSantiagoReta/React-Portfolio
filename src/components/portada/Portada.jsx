946f7f91cc9512b99b7ab5e8cfa3503f47d75b35
Guido Santiago Reta <guidoreta@hotmail.com>
2023-01-19 20:36:00 -0300

    typeitjs colors

--- src/components/portada/Portada.jsx
+++ src/components/portada/Portada.jsx
@@ -6,7 +6,7 @@ import css3 from"../../img/css3.png";
 import javascript from"../../img/javascript.png";
 import reacticon from "../../img/react.png";
 import Contact from "../contact/Contact.jsx";
-
+import TypeIt from "typeit-react";
 
 const Portada = () => {
   return (
@@ -14,7 +14,7 @@ const Portada = () => {
   
       <video className="video" src={port} autoPlay loop muted></video>
       <h1>Guido Santiago Reta</h1>
-     <p>Front-End Developer | JavaScript | React.js </p> 
+     <p><TypeIt>Front-End Developer | <span className="javascript">JavaScript</span> | <span className="react">React.js</span></TypeIt></p>
       <div className="icons-container">
         <img src={html5} id='rot' alt="html5"/>
         <img src={css3} id='inverse-rotation' alt="ccs3"/>
