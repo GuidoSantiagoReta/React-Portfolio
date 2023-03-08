6ed0965a18a91c216beb6ba61d58475f6391454c
Guido Santiago Reta <guidoreta@hotmail.com>
2023-03-08 17:11:08 -0300

    contact changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,13 @@
 
 import React from 'react'
 import './Contact.css'
-import { SiWhatsapp, SiLinkedin, SiGithub } from "react-icons/si";
+import { SiTwitter, SiLinkedin, SiGithub } from "react-icons/si";
 
 const Contact = () => {
   return (
     
         <div className='links'>
-            <a href="https://wa.me/541133232885" alt="whats app"><SiWhatsapp/></a>
+            <a href="https://twitter.com/guidoreta" alt="twitter"><SiTwitter/></a>
             <a href="https://www.linkedin.com/in/guido-reta" alt="linkedin"><SiLinkedin/></a>
             <a href="https://github.com/GuidoSantiagoReta" alt="github"><SiGithub/></a>
         </div>
