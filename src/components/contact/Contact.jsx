d8e53bad981bd046f2d2577bb6519227498dcfb3
GuidoSantiagoReta <guidoreta@hotmail.com>
2023-10-21 22:13:54 -0300

    portfolio 15

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,17 @@
 
-import React from 'react'
-import './Contact.css'
-import { SiTwitter, SiLinkedin, SiGithub } from "react-icons/si";
+import React from 'react';
+import './Contact.css';
+import { SiLinkedin, SiGithub, SiInstagram} from "react-icons/si";
 
 const Contact = () => {
   return (
     
         <div className='links'>
-            <a href="https://twitter.com/guidoreta" alt="twitter"><SiTwitter/></a>
+            <a href="https://www.instagram.com/s3guido" alt="instagram"><SiInstagram/></a>
             <a href="https://www.linkedin.com/in/guido-reta" alt="linkedin"><SiLinkedin/></a>
             <a href="https://github.com/GuidoSantiagoReta" alt="github"><SiGithub/></a>
         </div>
-      
+     
     
   )
 }
