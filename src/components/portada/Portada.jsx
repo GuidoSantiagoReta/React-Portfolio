109484b2c14b95e1b7d8fc6fe3b01ba53084d3e6
GuidoSantiagoReta <guidoreta@hotmail.com>
2023-11-28 12:08:04 -0300

    portfolio 25

--- src/components/portada/Portada.jsx
+++ src/components/portada/Portada.jsx
@@ -7,38 +7,33 @@ import javascript from"../../img/javascript.png";
 import reacticon from "../../img/react.png";
 import Contact from "../contact/Contact.jsx";
 import TypeIt from "typeit-react";
+import Modal from "../../components/modalProjects/Modal";
 import useModal from "../../hooks/useModal";
-
-
-const Modal = React.lazy(() => import("../../components/modalProjects/Modal"));
-const ProfileImage = React.lazy(() => import("../profileimage/ProfileImage.jsx"));
+import ProfileImage from "../profileimage/ProfileImage.jsx";
 
 const Portada = () => {
+
   const [isOpenModal, openModal, closeModal] = useModal();
   return (
-    <React.Suspense fallback={<div></div>}>
-      <div className="portada-container">
-        <video className="video" src={port} autoPlay loop muted preload="auto"></video>
-        <ProfileImage/>
-        <h1>Guido Santiago Reta</h1>
-        <p>
-          <TypeIt className="typeit-cursor" options={{loop: true}}>
-            <span className="html">Frontend</span> Developer | <span className="javascript">JavaScript</span> | <span className="react">React.js</span>
-          </TypeIt>
-        </p>
-        <div className="icons-container">
-          <img src={html5} id='rot' alt="html5"/>
-          <img src={css3} id='inverse-rotation' alt="ccs3"/>
-          <img src={javascript} id='rot' alt="javascrip"/>
-          <img src={reacticon} id='inverse-rotation' alt="react"/>
+    <div className="portada-container">
+  
+      <video className="video" src={port} autoPlay loop muted></video>
+      <ProfileImage/>
+      <h1>Guido Santiago Reta</h1>
+     <p><TypeIt className="typeit-cursor" options={{loop: true}}><span className="html">Frontend</span> Developer | <span className="javascript">JavaScript</span> | <span className="react">React.js</span></TypeIt></p>
+      <div className="icons-container">
+        <img src={html5} id='rot' alt="html5"/>
+        <img src={css3} id='inverse-rotation' alt="ccs3"/>
+        <img src={javascript} id='rot' alt="javascrip"/>
+        <img src={reacticon} id='inverse-rotation' alt="react"/>
         </div>
+        
         <button className="projects-btn hvr-pulse-shrink" onClick={openModal}>
-          <h4>Projects</h4>
-        </button>
-        <Modal isOpen={isOpenModal} closeModal={closeModal} />
-        <Contact/>
-      </div>
-    </React.Suspense>
+        <h4>Projects</h4>
+      </button>
+      <Modal isOpen={isOpenModal} closeModal={closeModal} />
+      <Contact/>
+    </div>
   )
 }
 
