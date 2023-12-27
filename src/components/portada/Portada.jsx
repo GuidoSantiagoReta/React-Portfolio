bac5262fdb57e2f6315368d98de97c8fe94d10ab
Guido Santiago Reta <guidoreta@hotmail.com>
2023-12-27 17:11:05 -0300

    portfolio 30

--- src/components/portada/Portada.jsx
+++ src/components/portada/Portada.jsx
@@ -5,6 +5,8 @@ import html5 from "../../img/html5.png";
 import css3 from"../../img/css3.png";
 import javascript from"../../img/javascript.png";
 import reacticon from "../../img/react.png";
+import node from "../../img/nodejs.png";
+import csharp from"../../img/csharp.png";
 import Contact from "../contact/Contact.jsx";
 import TypeIt from "typeit-react";
 import Modal from "../../components/modalProjects/Modal";
@@ -20,12 +22,14 @@ const Portada = () => {
       <video className="video" src={port} autoPlay loop muted preload="auto"></video>
       <ProfileImage/>
       <h1>Guido Santiago Reta</h1>
-     <p><TypeIt className="typeit-cursor" options={{loop: true}}><span className="html">Frontend</span> Developer | <span className="javascript">JavaScript</span> | <span className="react">React.js</span></TypeIt></p>
+     <p><TypeIt className="typeit-cursor" options={{loop: true}}><span className="html">Developer</span> | <span className="javascript">JavaScript</span> | <span className="react">React.js</span> | <span className="node">Node.js</span> | <span className="csharp">C#</span></TypeIt></p>
       <div className="icons-container">
         <img src={html5} id='rot' alt="html5"/>
         <img src={css3} id='inverse-rotation' alt="ccs3"/>
         <img src={javascript} id='rot' alt="javascrip"/>
         <img src={reacticon} id='inverse-rotation' alt="react"/>
+        <img src={node} id='rot' alt="node"/>
+        <img src={csharp} id='inverse-rotation' alt="csharp"/>
         </div>
         
         <button className="projects-btn hvr-pulse-shrink" onClick={openModal}>
