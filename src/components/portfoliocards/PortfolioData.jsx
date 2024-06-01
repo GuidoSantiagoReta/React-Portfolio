0f1259c24b88fbed0842282dd3bddda99fb60f30
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-06-01 14:27:55 -0300

    portfolioData

---
+++ src/components/portfoliocards/PortfolioData.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+import blogdecodigo from "../../assets/blogdecodigo.png";
+import estudiojuridico from "../../assets/estudioju2.png";
+import sistemasolar from "../../assets/sistemaSolar.png";
+import cannabisoil from "../../assets/cannabisoilarg.png";
+import swarerouter from "../../assets/swarerouterv6.png";
+import rickandmorty from "../../assets/rickandmorty.png";
+import shiftciclo from "../../assets/shiftciclo.png";
+import bancamv from "../../assets/banca.PNG";
+
+
+const portfolioData = [
+    {
+      image: bancamv,
+      altText: "BancaMV",
+      title: "BancaMV",
+      description: "SPA bancaria con Next, TypeScript y Tailwind.css",
+      demoUrl: "https://banca-mv.vercel.app/",
+      codeUrl: "https://github.com/GuidoSantiagoReta/BancaMV"
+    },
+    {
+      image: swarerouter,
+      altText: "Swaregrap routerv6",
+      title: "Swaregrap",
+      description: "Desarrollo Web SPA ReactRouterv6",
+      demoUrl: "https://swaregrap-reactrouterv6.vercel.app",
+      codeUrl: "https://github.com/GuidoSantiagoReta/Swaregrap-reactrouterv6"
+    },
+    {
+      image: sistemasolar,
+      altText: "APP Sistema Solar",
+      title: "APP Sistema Solar",
+      description: "PWA con React.js, Next.js y TypeScript",
+      demoUrl: "https://peit-react2-solar-system-app.vercel.app/",
+      codeUrl: "https://github.com/PrimerEmpleoIT/PEIT-react2-solar-system-app"
+    },
+    {
+      image: shiftciclo,
+      altText: "Shift.Ciclo",
+      title: "Desarrollo de SPA",
+      description: "Fiesta Shift.Ciclo",
+      demoUrl: "https://shift-ciclo.vercel.app/",
+      codeUrl: ""
+    },
+    {
+      image: estudiojuridico,
+      altText: "MPP Abogados",
+      title: "MPP Abogados",
+      description: "Desarrollo de Estudio Jurídico Integral",
+      demoUrl: "https://mppabogados.netlify.app",
+      codeUrl: "https://github.com/GuidoSantiagoReta/MPP-Abogados"
+    },
+    {
+      image: cannabisoil,
+      altText: "CannabisOilArg",
+      title: "Cannabis Oil Arg.",
+      description: "Venta de aceites medicinales con React.js",
+      demoUrl: "https://cannabis-oil-arg.vercel.app",
+      codeUrl: "https://github.com/GuidoSantiagoReta/Cannabis-Oil-Arg."
+    },
+    {
+      image: rickandmorty,
+      altText: "Rick and Morty",
+      title: "Rick and Morty App",
+      description: "Consumo de la API de Rick and Morty",
+      demoUrl: "https://rick-and-morty-app-pearl.vercel.app/",
+      codeUrl: "https://github.com/GuidoSantiagoReta/Rick-and-Morty-App"
+    },
+    {
+      image: blogdecodigo,
+      altText: "BlogDeCódigo",
+      title: "BlogDeCódigo",
+      description: "Blog personal realizado en React.js",
+      demoUrl: "https://blogdecodigo.vercel.app/",
+      codeUrl: "https://github.com/GuidoSantiagoReta/BlogDeCodigo"
+    }
+
+  ];
+  export default portfolioData;
