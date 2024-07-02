51d0939e33672861ea65c434b975725fb56527b9
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-07-02 13:38:50 -0300

    - Se actualizan proyectos

--- src/components/portfoliocards/PortfolioData.jsx
+++ src/components/portfoliocards/PortfolioData.jsx
@@ -8,139 +8,148 @@ import shiftciclo from "../../assets/shiftciclo.png";
 import bancamv from "../../assets/banca.PNG";
 import cosipatitas from "../../assets/cosipatitas.png";
 import inniarq from "../../assets/inniarq.png";
-import tasklist1 from"../../assets/tasklist1.png";
+import tasklist1 from "../../assets/tasklist1.png";
 import umsadevops from "../../assets/umsadevops.png"
 import blocdenotas from "../../assets/blocdenotas.png";
-import storev1  from "../../assets/storev1.png";
+import storev1 from "../../assets/storev1.png";
 import tdyd from "../../assets/tdyd.png";
+import httpclient from "../../assets/httpclient.png";
 
 const portfolioData = [
-    
-    {
-      image: cosipatitas,
-      altText: "CosiPatitas",
-      title: "CosiPatitas",
-      description: "Pet Shop SPA desarrollado con Angular 17",
-      demoUrl: "https://cosipatitas.vercel.app",
-      /*codeUrl: "https://github.com/GuidoSantiagoReta/CosiPatitasPetShop"*/
-    },
-    {
-      image: inniarq,
-      altText: "Inni Arquitectura",
-      title: "Inni Arquitectura",
-      description: "Estudio de arquitectura y diseño en desarrollo con React, Next, TypeScript y Tailwind.css.",
-      demoUrl: "https://inni-arquitectura.vercel.app/",
-     /* codeUrl: ""*/
-    },
-    {
-      image: bancamv,
-      altText: "BancaMV",
-      title: "BancaMV",
-      description: "Landing page bancaria Desarrollada con React, Next, TypeScript y Tailwind.css.",
-      demoUrl: "https://banca-mv.vercel.app/",
-      codeUrl: "https://github.com/GuidoSantiagoReta/BancaMV"
-    },
-    {
-      image: sistemasolar,
-      altText: "APP Sistema Solar",
-      title: "APP Sistema Solar",
-      description: " SPA y PWA colaborativa desarrollada con React.js, Next.js y TypeScript.",
-      demoUrl: "https://peit-react2-solar-system-app.vercel.app/",
-      codeUrl: "https://github.com/PrimerEmpleoIT/PEIT-react2-solar-system-app"
-    },
-    {
-      image: blogdecodigo,
-      altText: "BlogDeCódigo",
-      title: "BlogDeCódigo",
-      description: "Blog personal desarrollado con React + Context API.",
-      demoUrl: "https://blogdecodigo.vercel.app/",
-      codeUrl: "https://github.com/GuidoSantiagoReta/BlogDeCodigo"
-    },
-    {
-      image: tdyd,
-      altText: "TDYD",
-      title: "Tecnología, Diseño y Desarrollo",
-      description: "Estrategia de negocios, Desarrollo de producto y servicios Web con React.js.",
-      demoUrl: "https://tdyd.vercel.app/",
-      codeUrl: ""
-    },
-    {
-      image: storev1,
-      altText: "StoreV",
-      title: "StoreV",
-      description: "Módulo Ecommerce desarrollado con React, Axios + ReactRouterv6 + context API.",
-      /*demoUrl: "",*/
-      codeUrl: "https://github.com/GuidoSantiagoReta/storeV"
-    },
-    {
-      image: swarerouter,
-      altText: "Swaregrap",
-      title: "Swaregrap",
-      description: "Servicio técnico y desarrollo Web SPA ReactRouterv6.",
-      demoUrl: "https://swaregrap-reactrouterv6.vercel.app",
-      codeUrl: "https://github.com/GuidoSantiagoReta/Swaregrap-reactrouterv6"
-    },
-    {
-      image: estudiojuridico,
-      altText: "MPP Abogados",
-      title: "MPP Abogados",
-      description: "Estudio Jurídico Integral.",
-      demoUrl: "https://mppabogados.netlify.app",
-      /*codeUrl: "https://github.com/GuidoSantiagoReta/MPP-Abogados"*/
-    },
-    {
-      image: rickandmorty,
-      altText: "Rick and Morty",
-      title: "Rick and Morty App",
-      description: "Consumo de API Fetch + React.js.",
-      demoUrl: "https://rick-and-morty-app-pearl.vercel.app/",
-      codeUrl: "https://github.com/GuidoSantiagoReta/Rick-and-Morty-App"
-    },
-    {
-      image: blocdenotas,
-      altText: "APP BlocDeNotas",
-      title: "APP BlocDeNotas",
-      description: "APP de notas con React, Node.js,express, sequelize, MYSQL y dotenv.",
-     /*demoUrl: "https://github.com/GuidoSantiagoReta/BlocDeNotas",*/
-      codeUrl: "https://github.com/GuidoSantiagoReta/BlocDeNotas"
-    },
-    {
-      image: umsadevops,
-      altText: "Proyecto DevOps",
-      title: "Microservicio DevOps",
-      description: " Configuración de entornos de desarrollo y testing con Pipelines CI/CD, AWS, Terraform y Docker.",
-     /* demoUrl: "",*/
-      codeUrl: "https://github.com/alkemyTech/UMSA-DevOps-T4"
-    }
-    
-   /*
-   {
-      image: shiftciclo,
-      altText: "Shift.Ciclo",
-      title: "Fiesta Shift",
-      description: "SPA con React.js (en desarrollo).",
-      demoUrl: "https://shift-ciclo.vercel.app/",
-      codeUrl: ""
-    },
-   
-   {
-      image: tasklist1,
-      altText: "TO-DO List en React",
-      title: "TO-DO List React.js",
-      description: "Lista de tareas realizada en React.js",
-     demoUrl: "https://guidosantiagoreta.github.io/To-Do-List",
-      codeUrl: "https://github.com/GuidoSantiagoReta/To-Do-List"
-    }, 
-    {
-      image: cannabisoil,
-      altText: "CannabisOilArg",
-      title: "Cannabis Oil Arg.",
-      description: "Venta de aceites medicinales con React.js",
-      demoUrl: "https://cannabis-oil-arg.vercel.app",
-      codeUrl: "https://github.com/GuidoSantiagoReta/Cannabis-Oil-Arg."
-    },
-    */
-    
+  {
+    image: httpclient,
+    altText: "httpClient",
+    title: "Angular API HttpClient",
+    description: "Consumo de API de Productos con Angular, HttpClient, RxJS y Observables",
+    demoUrl: "https://http-client-angular.vercel.app/",
+    codeUrl: "https://github.com/GuidoSantiagoReta/http-client-angular"
+  },
 
-  ];
-  export default portfolioData;
+  {
+    image: cosipatitas,
+    altText: "CosiPatitas",
+    title: "CosiPatitas",
+    description: "Pet Shop SPA desarrollado con Angular 17",
+    demoUrl: "https://cosipatitas.vercel.app",
+    /*codeUrl: "https://github.com/GuidoSantiagoReta/CosiPatitasPetShop"*/
+  },
+  {
+    image: inniarq,
+    altText: "Inni Arquitectura",
+    title: "Inni Arquitectura",
+    description: "Estudio de arquitectura y diseño en desarrollo con React, Next, TypeScript y Tailwind.css.",
+    demoUrl: "https://inni-arquitectura.vercel.app/",
+    /* codeUrl: ""*/
+  },
+  {
+    image: bancamv,
+    altText: "BancaMV",
+    title: "BancaMV",
+    description: "Landing page bancaria Desarrollada con React, Next, TypeScript y Tailwind.css.",
+    demoUrl: "https://banca-mv.vercel.app/",
+    codeUrl: "https://github.com/GuidoSantiagoReta/BancaMV"
+  },
+  {
+    image: sistemasolar,
+    altText: "APP Sistema Solar",
+    title: "APP Sistema Solar",
+    description: " SPA y PWA colaborativa desarrollada con React.js, Next.js y TypeScript.",
+    demoUrl: "https://peit-react2-solar-system-app.vercel.app/",
+    codeUrl: "https://github.com/PrimerEmpleoIT/PEIT-react2-solar-system-app"
+  },
+  {
+    image: blogdecodigo,
+    altText: "BlogDeCódigo",
+    title: "BlogDeCódigo",
+    description: "Blog personal desarrollado con React + Context API.",
+    demoUrl: "https://blogdecodigo.vercel.app/",
+    codeUrl: "https://github.com/GuidoSantiagoReta/BlogDeCodigo"
+  },
+  {
+    image: storev1,
+    altText: "StoreV",
+    title: "StoreV",
+    description: "Módulo Ecommerce desarrollado con React, Axios + ReactRouterv6 + context API.",
+    /*demoUrl: "",*/
+    codeUrl: "https://github.com/GuidoSantiagoReta/storeV"
+  },
+  {
+    image: swarerouter,
+    altText: "Swaregrap",
+    title: "Swaregrap",
+    description: "Servicio técnico y desarrollo Web SPA ReactRouterv6.",
+    demoUrl: "https://swaregrap-reactrouterv6.vercel.app",
+    codeUrl: "https://github.com/GuidoSantiagoReta/Swaregrap-reactrouterv6"
+  },
+  {
+    image: estudiojuridico,
+    altText: "MPP Abogados",
+    title: "MPP Abogados",
+    description: "Estudio Jurídico Integral.",
+    demoUrl: "https://mppabogados.netlify.app",
+    /*codeUrl: "https://github.com/GuidoSantiagoReta/MPP-Abogados"*/
+  },
+  {
+    image: rickandmorty,
+    altText: "Rick and Morty",
+    title: "Rick and Morty App",
+    description: "Consumo de API Fetch + React.js.",
+    demoUrl: "https://rick-and-morty-app-pearl.vercel.app/",
+    codeUrl: "https://github.com/GuidoSantiagoReta/Rick-and-Morty-App"
+  },
+  {
+    image: blocdenotas,
+    altText: "APP BlocDeNotas",
+    title: "APP BlocDeNotas",
+    description: "APP de notas con React, Node.js,express, sequelize, MYSQL y dotenv.",
+    /*demoUrl: "https://github.com/GuidoSantiagoReta/BlocDeNotas",*/
+    codeUrl: "https://github.com/GuidoSantiagoReta/BlocDeNotas"
+  },
+  {
+    image: umsadevops,
+    altText: "Proyecto DevOps",
+    title: "Microservicio DevOps",
+    description: " Configuración de entornos de desarrollo y testing con Pipelines CI/CD, AWS, Terraform y Docker.",
+    /* demoUrl: "",*/
+    codeUrl: "https://github.com/alkemyTech/UMSA-DevOps-T4"
+  }
+
+  /*
+  {
+     image: shiftciclo,
+     altText: "Shift.Ciclo",
+     title: "Fiesta Shift",
+     description: "SPA con React.js (en desarrollo).",
+     demoUrl: "https://shift-ciclo.vercel.app/",
+     codeUrl: ""
+   },
+  
+  {
+     image: tasklist1,
+     altText: "TO-DO List en React",
+     title: "TO-DO List React.js",
+     description: "Lista de tareas realizada en React.js",
+    demoUrl: "https://guidosantiagoreta.github.io/To-Do-List",
+     codeUrl: "https://github.com/GuidoSantiagoReta/To-Do-List"
+   }, 
+   {
+     image: cannabisoil,
+     altText: "CannabisOilArg",
+     title: "Cannabis Oil Arg.",
+     description: "Venta de aceites medicinales con React.js",
+     demoUrl: "https://cannabis-oil-arg.vercel.app",
+     codeUrl: "https://github.com/GuidoSantiagoReta/Cannabis-Oil-Arg."
+   },
+    {
+     image: tdyd,
+     altText: "TDYD",
+     title: "Tecnología, Diseño y Desarrollo",
+     description: "Estrategia de negocios, Desarrollo de producto y servicios Web con React.js.",
+     demoUrl: "https://tdyd.vercel.app/",
+     codeUrl: ""
+   },
+   */
+
+
+];
+export default portfolioData;
