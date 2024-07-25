a079b67398332587b189f3a5a393cf4933f9bc78
GuidoSantiagoReta <guidosantiagoreta@gmail.com>
2024-07-25 11:27:49 -0300

    -Se modifica enlace a email de contacto

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react';
 import './Contact.css';
-import { SiLinkedin, SiGithub, SiInstagram } from "react-icons/si";
+import { SiLinkedin, SiGithub} from "react-icons/si";
+import { TfiEmail } from "react-icons/tfi";
 
 const Contact = () => {
   return (
 
     <div className='links'>
-      <a href="https://www.instagram.com/s3guido" alt="instagram"><SiInstagram /></a>
+      <a href="mailto:guidosantiagoreta@gmail.com" alt="correo electrónico"><TfiEmail /></a>
       <a href="https://www.linkedin.com/in/guido-reta" alt="linkedin"><SiLinkedin /></a>
       <a href="https://github.com/GuidoSantiagoReta" alt="github"><SiGithub /></a>
     </div>
